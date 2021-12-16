27284a6826ee377b8e68a3b853bdcae505057159
rajat.lahariyacredgenics.com <rajat.lahariyacredgenics.com@localhost>
2021-12-16 13:53:34 +0530

    just removed some unused imports

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -4,11 +4,9 @@ package pages;
 
 import java.util.List;
 import java.util.Set;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WindowType;
 import org.openqa.selenium.support.FindBy;
 import base.PageBase;
 import utils.TestUtil;
